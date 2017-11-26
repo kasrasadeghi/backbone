e0a8ef0e7a77ef980903244e94a24cb776842c0a
willsherwood <will.sherwood5@gmail.com>
2017-11-26 17:32:02 -0600

    Implement mapping

---
+++ map.h
@@ -0,0 +1,17 @@
+#include "parser.h"
+#include <stddef.h>
+
+typedef struct Entry {
+    char *key;
+    Sexp *s;
+} Entry;
+
+typedef struct Map {
+    Entry *entries;
+    size_t size;
+    size_t capacity;
+} Map;
+
+Map *map_create();
+Sexp *map_get(Map *map, char *key);
+void map_put(Map *map, char *key, Sexp *s);
