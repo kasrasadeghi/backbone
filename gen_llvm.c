86ec310ac829210e4e20b9a3b7ead95401624a39
kasrasadeghi <kasra.sadeghi1@gmail.com>
2017-11-29 12:42:18 -0600

    generate str-table. Need atom-based strlen

--- gen_llvm.c
+++ gen_llvm.c
@@ -4,8 +4,16 @@
 #include <unistd.h>
 #include "gen_llvm.h"
 
-void gStrTable(FILE* file, Sexp* s) {
+size_t atomStrLen(char* s) {
   //TODO
+}
+
+void gStrTable(FILE* file, Sexp* s) {
+  for (int i = 0; i < s->length; ++i) {
+    char* string_entry = s->list[i]->list[0]->value;
+    printf("@str.%d = private unnamed_addr constant [%lu x i8] c%s, align 1",
+           i, atomStrLen(string_entry), string_entry);
+  }
 }
 
 void gStruct(FILE* file, Sexp* s) {
@@ -17,6 +25,9 @@ void gDef(FILE* file, Sexp* s) {
 }
 
 void gProgram(FILE* file, Sexp* s) {
+  printf("; ModuleID = %s\n", s->value);
+  printf("target datalayout = \"e-m:e-i64:64-f80:128-n8:16:32:64-S128\""
+      "\ntarget triple = \"x86_64-unknown-linux-gnu\"\n");
   for (int i = 0; i < s->length; ++i) {
     Sexp* child = s->list[i];
     if (strcmp(child->value, "str-table") == 0) {
