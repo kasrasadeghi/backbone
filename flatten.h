c466f82bc0c4dd0a50971d39dab6f074be739acb
kasrasadeghi <kasra.sadeghi1@gmail.com>
2017-12-07 15:37:13 -0600

    documentation for flatten.h

--- flatten.h
+++ flatten.h
@@ -2,14 +2,22 @@
 #ifndef __FLATTEN_H__
 #define __FLATTEN_H__
 
+/**
+ * The 'flatten' pass takes nested expressions and flattens them
+ * to one expression evaluation per statement. Inner expressions are
+ * extracted to 'let' statements. Expressions that are expected to be
+ * values (literals or registers) are also extracted.
+ *
+ * This header file also provides an API for common string comparison
+ * operations in Sexp's.
+ */
+
 #include "parser.h"
 
 void flatten(Sexp* p);
 
 int isCall(Sexp* s);
-
 int isAdd(Sexp* s);
-
 int isIcmp(Sexp* s);
 
 #endif
