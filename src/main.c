5399f8d445b373cfeb8f8ecfc28cf0b1022dcda5
kasrasadeghi <kasra.sadeghi1@gmail.com>
2018-01-05 16:48:05 -0600

    update usage str

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 
 int main(int argc, char *argv[]) {
   if (argc < 3) {
-    puts("Usage: backbone <parse|generateLLVM> <file>");
+    puts("Usage: backbone <parse|flatten|qualify|gen> <file>");
     return 1;
   }
   char* filename = argv[2];
