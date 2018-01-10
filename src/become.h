03f9179d2d08792bf4510e4d13e46d842df78970
kasrasadeghi <kasra.sadeghi1@gmail.com>
2018-01-10 13:21:02 -0600

    become and callstmt documentation

--- src/become.h
+++ src/become.h
@@ -4,6 +4,17 @@
 
 #include "sexp.h"
 
+/**
+ * (become 'name 'types 'return-type 'args)
+ *
+ * if 'return-type == void
+ *  => (call-tail 'name 'types 'return-type 'args)
+ *     (return void)
+ *
+ * otherwise
+ *     (return (call-tail 'name 'types 'return-type 'args) 'return-type)
+ *
+ */
 void become(Sexp* p);
 
 #endif
--- src/callstmt.h
+++ src/callstmt.h
@@ -1,10 +1,23 @@
 
-
 #ifndef __CALL_STMT_H__
 #define __CALL_STMT_H__
 
 #include "sexp.h"
 
+/**
+ * Transforms expr-calls (calls that do not return void) to be statements.
+ * Is a no-op on calls that return void.
+ *
+ * ('CallLike 'name 'types 'return-type 'args)
+ *
+ * if 'return-type == void
+ * => ('CallLike 'name 'types 'return-type 'args)
+ *
+ * else
+ * => (let 'new-local ('CallLike 'name 'types 'return-type 'args)
+ *
+ * @param p - a program with calls in it.
+ */
 void callStmt(Sexp* p);
 
 #endif
